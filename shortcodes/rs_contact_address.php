dfef484d858c67140bf139de294e35de8e5987ef
asiancity <ath@strachwitzgerhard.de>
2017-02-22 16:24:00 +0100

    update contact address

--- shortcodes/rs_contact_address.php
+++ shortcodes/rs_contact_address.php
@@ -1,6 +1,7 @@
 <?php
 function rs_contact_address(){
   $phone      = get_theme_mod('phone');
+  $fax        = get_theme_mod('fax');
   $mail       = get_theme_mod('mail');
   $blogname   = get_bloginfo('name');
   $address    = get_theme_mod('address');
@@ -8,8 +9,9 @@ function rs_contact_address(){
   $city       = get_theme_mod('city');
 
   $out   = '<div class="contact">';
-  $out  .= '<div class="media"><i class="fa fa-home pull-left"></i><div class="media-body">'.$blogname.'<br />'.$address.'<br />'.$postcode.' '.$city.'</div></div>';
+  $out  .= '<div class="media address"><i class="fa fa-home pull-left"></i><div class="media-body"><h3>'.$blogname.'</h3>'.$address.'<br />'.$postcode.' '.$city.'</div></div>';
   $out  .= '<div class="media"><i class="fa fa-phone pull-left"></i><div class="media-body">'.$phone.'</div></div>';
+  $out  .= '<div class="media"><i class="fa fa-fax pull-left"></i><div class="media-body">'.$fax.'</div></div>';
   $out  .= '<div class="media"><i class="fa fa-envelope-o pull-left"></i><div class="media-body">'.$mail.'</div></div>';
   $out  .= '</div>';
   return $out;
